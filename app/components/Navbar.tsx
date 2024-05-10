70247a42350d6149bcfb54d975f75176dce2e90e
Jatesh Parikh <jateshparikh97@gmail.com>
2024-05-10 20:12:29 +0530

    products page completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,7 +15,7 @@ const links = [
 export default function Navbar() {
   const pathname = usePathname();
   return (
-    <header className="mb-8 border-b p-2">
+    <header className="bg-white mb-8 border-b p-2 sticky top-0 z-50">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href={"/"}>
           <h1 className="text-2xl md:text-4xl font-bold">
